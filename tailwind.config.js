db375e702ff5a1bffcfd76bc3e19de9fbb762e21
Hemudi <ksum1205@naver.com>
2023-07-04 14:52:35 +0900

    :wrench: Tailwind purge 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  purge: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
   },
