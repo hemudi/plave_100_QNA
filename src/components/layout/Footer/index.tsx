25fcb4dfd17dbcac9d4ced438c4e12dfad57550d
Hemudi <ksum1205@naver.com>
2023-07-07 15:56:33 +0900

    :lipstick: BodyLayout UI 컴포넌트 구현 및 story 파일 추가

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,8 +1,8 @@
-const AUTHOR_NICKNAME = 'Heasam Smoothie';
+const AUTHOR_NICKNAME = '해삼스무디';
 
 const Footer = () => {
   return (
-    <footer className="flex h-14 w-full select-none items-center justify-center text-sm text-slate-400">
+    <footer className="flex h-14 w-full select-none items-center justify-center text-xs text-slate-500">
       <span>{`Created By. ${AUTHOR_NICKNAME}`}</span>
     </footer>
   );
