59b080afa126697a2fbe85c4339602ee36da8b33
Hemudi <ksum1205@naver.com>
2023-07-07 15:20:35 +0900

    :lipstick: Header ui 색상 변경

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <header className="flex h-11 w-full select-none items-center justify-center rounded-t-2xl bg-neutral-100 px-4">
       <div className="flex items-center gap-1">
-        <div className="h-3 w-3 rounded-xl bg-red-500" />
-        <div className="h-3 w-3 rounded-xl bg-yellow-500" />
-        <div className="h-3 w-3 rounded-xl bg-green-500" />
+        <div className="h-3 w-3 rounded-xl bg-red-500/80" />
+        <div className="h-3 w-3 rounded-xl bg-yellow-500/80" />
+        <div className="h-3 w-3 rounded-xl bg-green-500/80" />
       </div>
       <div className="flex grow justify-center">
         <h1>{HEADER_TITLE}</h1>
